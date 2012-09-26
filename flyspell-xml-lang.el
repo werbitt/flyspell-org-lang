65fee045fd1cb610e0f2564008bbab4393f420b0
Peter Heslin <p.j.heslin@dur.ac.uk>
2012-09-26 12:36:07 +0200

    updates

--- flyspell-xml-lang.el
+++ flyspell-xml-lang.el
@@ -4,7 +4,7 @@
 ;; Copyright (C) 2004, 2005 P J Heslin
 ;;
 ;; Author: Peter Heslin <p.j.heslin@dur.ac.uk>
-;; URL: http://www.dur.ac.uk/p.j.heslin/Software/Emacs/Download/flyspell-xml-lang.el
+;; URL: http://www.dur.ac.uk/p.j.heslin/Software/Emacs
 ;; Version: 2.1
 ;;
 ;; This program is free software; you can redistribute it and/or modify
